428ec2ddedec1329bcdf0290fe3ea01942e097e7
Robbie Belson <rhbelson@gmail.com>
2018-11-11 14:25:33 -0600

    notifications deployed, activity changes update icons

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,6 +9,7 @@ import { LocalNotifications } from '@ionic-native/local-notifications';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
+
 })
 export class HomePage {
 
@@ -24,6 +25,12 @@ export class HomePage {
 //         console.log("Oops!");
 //     }
 // );
+  // this.inputValue="";
+}
+
+
+learnMore() {
+  this.addNotification();
 }
 
 
@@ -33,14 +40,13 @@ addNotification() {
   this.localNotifications.schedule({
     id: 1,
     text: 'Reminder! Do Your Job!',
-    sound: isAndroid? 'file://sound.mp3': 'file://beep.caf',
-    trigger: {at: new Date(new Date().getTime() + 7000)},
-    data: { secret: key }
+    trigger: {at: new Date(new Date().getTime() + 10)},
   });
 }
 
 
 presentLoadingText() {
+  // console.log(this.inputValue)
   let loading = this.loadingCtrl.create({
     spinner: 'hide',
     content: 'Loading Please Wait...'
@@ -57,6 +63,7 @@ presentLoadingText() {
      // Data Processing
   document.getElementById('alert_card').style.display="none";
   var target_email;
+  // target_email=this.inputValue;
   target_email=document.querySelectorAll("#user_email>input")[0].value;
   console.log(target_email);
 
@@ -100,13 +107,53 @@ setTimeout(function(){
       document.getElementById("activity_2").innerHTML=words[2];
       document.getElementById("activity_3").innerHTML=words[3];
       document.getElementById("activity_4").innerHTML=words[4];
+      document.getElementById("activity_5").innerHTML=words[5];
+      document.getElementById("activity_6").innerHTML=words[6];
+      document.getElementById("activity_7").innerHTML=words[7];
+      document.getElementById("activity_8").innerHTML=words[8];
+      // document.getElementById("activity_9").innerHTML=words[9];
+      // document.getElementById("activity_10").innerHTML=words[10];
+      // document.getElementById("activity_11").innerHTML=words[11];
+      // document.getElementById("activity_12").innerHTML=words[12];
+      // document.getElementById("activity_13").innerHTML=words[13];
+      // document.getElementById("activity_14").innerHTML=words[14];
+      // document.getElementById("activity_15").innerHTML=words[15];
+      
+    //Update Icons with for loop by activity name
+    var j;
+    for (j=1; j<9; j++) {
+      var activity_iter="activity_".concat(j.toString());
+      var icon_iter="icon".concat(j.toString());
+      console.log("Activity being changed: "+activity_iter);
+      if (document.getElementById(activity_iter).innerHTML.toLowerCase()=="footrub") {
+        document.getElementById(icon_iter).className="icon icon-md ion-md-medkit";
+       }
+
+      else if (document.getElementById(activity_iter).innerHTML.toLowerCase()=="free") {
+          document.getElementById(icon_iter).className="icon icon-md ion-md-happy";
+       }
+
+       else if (document.getElementById(activity_iter).innerHTML.toLowerCase()=="food") {
+          document.getElementById(icon_iter).className="icon icon-md ion-md-pizza";
+       }
+
+       else if (document.getElementById(activity_iter).innerHTML.toLowerCase()=="security") {
+          document.getElementById(icon_iter).className="icon icon-md ion-md-eye";
+       }
+
+       else if (document.getElementById(activity_iter).innerHTML.toLowerCase()=="bathroom runner") {
+          document.getElementById(icon_iter).className="icon icon-md ion-md-water";
+       }
+    }
+
     }
   }
 
   if (email_found==false) {
     document.getElementById('alert_card').style.display="block";
+    document.getElementById("fullCalendar").style.display="none";
   }
-  else {
+  else { 
     //Lastly, display calendar
     document.getElementById('alert_card').style.display="none";
     document.getElementById("fullCalendar").style.display="block";
@@ -115,94 +162,6 @@ setTimeout(function(){
    }, 3000);
 
 }
-
-	
-
-  // fireAlert() {
-  //     let alert = this.alertController.create({
-  //         title: 'Error!',
-  //         subTitle: 'Sorry, that email does not appear in our directory',
-  //         buttons: ['OK']
-  //     });
-
-  //     alert.present();
-  // }
- 
-
-  // adjustCalUI() {
-  // 	//for each activity, read the text (e.g., footrub) and adjust icon accordingly..
-  //   if (document.getElementById("activity_1").innerHTML=="footrub") {
-
-  //   }
-  // }
-
-
-  viewSchedule() {
-        // Data Processing
-        document.getElementById('alert_card').style.display="none";
-        var target_email;
-        target_email=document.querySelectorAll("#user_email>input")[0].value;
-        console.log(target_email);
-
-
-        var txt = '';
-  		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.onreadystatechange = function(){
-		  if(xmlhttp.status == 200 && xmlhttp.readyState == 4){
-		    txt = xmlhttp.responseText;
-		    console.log(txt);
-		  }
-		};
-		xmlhttp.open("GET","./dm_cal.csv",true);
-		xmlhttp.send();
-
-
-		// var target_email="bob@example.com";
-		//Parse Input
-		setTimeout(function(){ 
-			console.log("parsing input.."); 
-			var lines=txt.split('\n');
-        	console.log(lines[0]);
-
-
-        ///Step 1: Find Correct User Email
-        	var i;
-        	var email_found=false;
-        	for (i=0;i<lines.length;i++) {
-        		var words=lines[i].split(',');
-        		if (words[0]==target_email) {
-        			console.log("found email :"+ words[0])
-        			email_found=true;
-        			//Fill in calendar (for however many activities exist...)
-	        		document.getElementById("activity_1").innerHTML=words[1];
-	        		document.getElementById("activity_2").innerHTML=words[2];
-	        		document.getElementById("activity_3").innerHTML=words[3];
-	        		document.getElementById("activity_4").innerHTML=words[4];
-        		}
-        	}
-
-        	if (email_found==false) {
-            document.getElementById('alert_card').style.display="block";
-        		// Alert not working
-			    // alert.present();
-            // fireAlert();
-        	}
-        	else {
-	        	//Lastly, display calendar
-            document.getElementById('alert_card').style.display="none";
-	        	document.getElementById("fullCalendar").style.display="block";
-	        	email_found=false;
-        	}
-        	
-
-        }, 3000);
-
-    addNotification();
-
-    }
-
-
-
 
   // getPosts() {
   //   return  this.http.get(this.getApiUrl)
