a67f595dc5be06a8c57315f480356d883821dab3
Robbie Belson <rhbelson@gmail.com>
2018-11-17 11:28:37 -0600

    parses activities and inNorris

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -16,22 +16,50 @@ export class AboutPage {
 		//   console.log(response.error);
 		//   console.log("Not so good..");
 		// });
-this.http.get('http://hinckley.cs.northwestern.edu/~rbi054/get.php', {}, {})
-  .then(data => {
+	var response="";
+	this.http.get('http://hinckley.cs.northwestern.edu/~rbi054/get.php', {}, {})
+	  .then(data => {
 
-    console.log(data.status);
-    console.log(data.data); // data received by server
-    console.log(data.headers);
+	    console.log(data.status);
+	    // console.log(data.data); // data received by server
+	    response=data.data;
+	    console.log(data.headers);
 
-  })
-  .catch(error => {
+	  })
+	  .catch(error => {
 
-    console.log(error.status);
-    console.log(error.error); // error message as string
-    console.log(error.headers);
+	    console.log(error.status);
+	    console.log(error.error); // error message as string
+	    console.log(error.headers);
+	  });
 
-  });
-	 console.log("Hello world!");
+	setTimeout(function(){ 
+		var text=response.split('\n');
+		var activities=[];
+		var norris_count=[];
+		console.log("Parsing dm_info.txt dataset..");
+
+		//Get Activities
+		var i;
+		for (i=0;i<text.length;i++) {
+			//Get Activity
+			var activity_beg=text[i].indexOf("userActivity");
+			activities.push(text[i].substring(activity_beg+15,text[i].length-1));
+
+			//Get Norris
+			var norris_beg=text[i].indexOf("inNorris");
+			var norris_end=text[i].indexOf("timestamp");
+			norris_count.push(text[i].substring(norris_beg+10,norris_end-3));
+		}
+		console.log(activities);
+		console.log(norris_count);
+
+
+
+
+	 }, 4000);
+	
+
   }
 
 
