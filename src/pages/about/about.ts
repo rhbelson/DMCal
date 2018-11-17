3819a22ea52b10248c6a01aab756f37dd8291d0e
Robbie Belson <rhbelson@gmail.com>
2018-11-16 20:50:15 -0600

    query database entry for member info

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HTTP } from '@ionic-native/http';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,31 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: HTTP) {
+	 // this.http.get('http://hinckley.cs.northwestern.edu/~rbi054/get.php', function(response) {
+	 // 		console.log("Good!");
+		//   console.log(response);
+		// }, function(response) {
+		//   console.log(response.error);
+		//   console.log("Not so good..");
+		// });
+this.http.get('http://hinckley.cs.northwestern.edu/~rbi054/get.php', {}, {})
+  .then(data => {
 
+    console.log(data.status);
+    console.log(data.data); // data received by server
+    console.log(data.headers);
+
+  })
+  .catch(error => {
+
+    console.log(error.status);
+    console.log(error.error); // error message as string
+    console.log(error.headers);
+
+  });
+	 console.log("Hello world!");
   }
+
 
 }
